868ca6fa0224f6896e5b3eac523170817fd5d288
mark reyes <mark.reyes@ucop.edu>
2015-11-04 12:01:42 -0800

    Create Inventory development configs

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,8 +3,8 @@
 JAVA_HOME=/cdlcommon/products/java7
 JRE_HOME=/cdlcommon/products/java7
 CATALINA_HOME=/cdlcommon/products/apache-tomcat
-CATALINA_BASE=/apps/dpr2/apps/PLACE_HOLDER_FOR_SERVICE_NAME/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms512m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true"
-CATALINA_PID=${CATALINA_BASE}/PLACE_HOLDER_FOR_SERVICE_NAME.pid
+CATALINA_BASE=/apps/dpr2/apps/inv36121/tomcat
+CATALINA_OPTS="-Xmx512m -Xms256m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true"
+CATALINA_PID=${CATALINA_BASE}/inv36121.pid
 # CLASSPATH=${CATALINA_HOME}/bin/tomcat-juli.jar
 export JAVA_HOME JRE_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID CLASSPATH
