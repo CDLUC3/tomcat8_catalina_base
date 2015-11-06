4e62754091647780a2c886eab12a32427a332f8d
mark reyes <mark.reyes@ucop.edu>
2015-11-06 14:52:46 -0800

    Fix Tomcat PID

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,8 +3,9 @@
 JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
-CATALINA_BASE=/apps/dpr2/apps/ingest33121/tomcat
+CATALINA_ROOT=/apps/dpr2/apps/ingest33121
+CATALINA_BASE=${CATALINA_ROOT}/tomcat
 CATALINA_OPTS="-Xmx512m -Xms256m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true"
-CATALINA_PID=${CATALINA_BASE}/ingest.pid
+CATALINA_PID=${CATALINA_ROOT}/ingest.pid
 # CLASSPATH=${CATALINA_HOME}/bin/tomcat-juli.jar
 export JAVA_HOME JRE_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID CLASSPATH
