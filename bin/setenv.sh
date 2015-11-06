725ca9576895f0e876ea2d8a2bc068520091125e
mark reyes <mark.reyes@ucop.edu>
2015-11-06 15:50:54 -0800

    update too David's working version

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,10 +1,9 @@
-#! /bin/sh
-
-JAVA_HOME=/cdlcommon/products/java7
-JRE_HOME=/cdlcommon/products/java7
-CATALINA_HOME=/cdlcommon/products/apache-tomcat
+# JAVA_HOME=/cdlcommon/products/java7
+# needed for G1 collector
+JAVA_HOME=//usr/lib/jvm/java
+JRE_HOME=//usr/lib/jvm/java
+CATALINA_HOME=/dpr2/tomcat
 CATALINA_BASE=/apps/dpr2/apps/inv36121/tomcat
-CATALINA_OPTS="-Xmx512m -Xms256m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true"
-CATALINA_PID=${CATALINA_BASE}/inv36121.pid
-# CLASSPATH=${CATALINA_HOME}/bin/tomcat-juli.jar
-export JAVA_HOME JRE_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID CLASSPATH
+CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
+CATALINA_PID=/dpr2/apps/inv36121/tomcat/catalina.pid
+export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
