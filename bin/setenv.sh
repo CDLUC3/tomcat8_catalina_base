5fa118f64327af5ec62e43ae565debe1a40a53e8
mark reyes <mark.reyes@ucop.edu>
2015-11-06 16:17:37 -0800

    standarize PID

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,5 +5,5 @@ JRE_HOME=//usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
 CATALINA_BASE=/apps/dpr2/apps/inv36121/tomcat
 CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
-CATALINA_PID=/dpr2/apps/inv36121/tomcat/catalina.pid
+CATALINA_PID=/dpr2/apps/inv36121/inv.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
