1b9ebae30ab95ddf444812da048177df47a79add
mark reyes <mark.reyes@ucop.edu>
2015-11-10 13:07:42 -0800

    alter PID location and name

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,5 +5,5 @@ JRE_HOME=//usr/lib/jvm/java
 CATALINA_HOME=/dpr2store/tomcat
 CATALINA_BASE=/apps/dpr2store/apps/storage35121/tomcat
 CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
-CATALINA_PID=/dpr2store/apps/storage35121/tomcat/catalina.pid
+CATALINA_PID=/dpr2store/apps/storage35121/storage.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
