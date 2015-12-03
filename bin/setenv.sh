9ef417d1b6bd32462f2c6f9d97c26ba9d5c3a026
mark reyes <mark.reyes@ucop.edu>
2015-12-02 17:57:43 -0800

    dataone dev branch

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,9 +1,10 @@
 # JAVA_HOME=/cdlcommon/products/java6
 # needed for G1 collector
-JAVA_HOME=//usr/lib/jvm/java
-JRE_HOME=//usr/lib/jvm/java
-CATALINA_HOME=/dpr2store/tomcat
-CATALINA_BASE=/apps/dpr2store/apps/storage35121/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
-CATALINA_PID=/dpr2store/apps/storage35121/storage.pid
+JAVA_HOME=/usr/lib/jvm/java
+JRE_HOME=/usr/lib/jvm/java
+CATALINA_HOME=/dataone/tomcat
+CATALINA_ROOT=/apps/dataone/apps/metacat33181
+CATALINA_BASE=${CATALINA_ROOT}/tomcat
+CATALINA_OPTS="-Xmx512m -Xms356m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
+CATALINA_PID=${CATALINA_ROOT}/metacat.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
