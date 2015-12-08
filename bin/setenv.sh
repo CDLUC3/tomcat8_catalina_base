dfe9ef525b0f362ab9a466299b83ac520eab6258
mark reyes <mark.reyes@ucop.edu>
2015-12-08 11:31:52 -0800

    fine tune

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,6 +5,6 @@ JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dataone/tomcat
 CATALINA_ROOT=/apps/dataone/apps/metacat33181
 CATALINA_BASE=${CATALINA_ROOT}/tomcat
-CATALINA_OPTS="-Xmx512m -Xms356m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
+CATALINA_OPTS="-Xmx512m -Xms256m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC"
 CATALINA_PID=${CATALINA_ROOT}/metacat.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
