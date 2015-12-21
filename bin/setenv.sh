8b9c202cf3a429e307916c5cb03d0f84b94c1e81
mark reyes <mark.reyes@ucop.edu>
2015-12-21 10:02:22 -0800

    Replication dev under capistrano deployment

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,7 +3,7 @@
 JAVA_HOME=//usr/lib/jvm/java
 JRE_HOME=//usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
-CATALINA_BASE=/apps/dpr2/apps/inv36121/tomcat
-CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
-CATALINA_PID=/dpr2/apps/inv36121/inv.pid
+CATALINA_BASE=/apps/dpr2/apps/replic38001/tomcat
+CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
+CATALINA_PID=/dpr2/apps/replic38001/replic.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
