b812015234f8a9523614f9344f84a45455a14292
mark reyes <mark.reyes@ucop.edu>
2016-01-19 10:59:50 -0800

    64 bit java

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,6 +5,6 @@ JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dataone/tomcat
 CATALINA_ROOT=/apps/dataone/apps/metacat33181
 CATALINA_BASE=${CATALINA_ROOT}/tomcat
-CATALINA_OPTS="-Xmx512m -Xms256m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC"
+CATALINA_OPTS="-Xmx512m -Xms256m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=${CATALINA_ROOT}/metacat.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
