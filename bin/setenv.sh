37048a11ba1c280cc9ff7eec5d3d398d77d73a89
mark reyes <mark.reyes@ucop.edu>
2016-01-19 10:57:23 -0800

    64 bit java

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,9 +1,9 @@
 # JAVA_HOME=/cdlcommon/products/java7
 # needed for G1 collector
-JAVA_HOME=//usr/lib/jvm/java
-JRE_HOME=//usr/lib/jvm/java
+JAVA_HOME=/usr/lib/jvm/java
+JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
 CATALINA_BASE=/apps/dpr2/apps/inv36121/tomcat
-CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -verbose:gc -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+UseG1GC"
+CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2/apps/inv36121/inv.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
