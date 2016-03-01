2ba6e41f84198456580031819846cccb3f987821
mark reyes <mark.reyes@ucop.edu>
2016-03-01 10:48:11 -0800

    node 01 prod

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,7 +3,7 @@
 JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
-CATALINA_BASE=/apps/dpr2/apps/node35801/tomcat
-CATALINA_OPTS="-Xmx1048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_BASE=/dpr2/apps/node35801/tomcat
+CATALINA_OPTS="-Xmx2048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2/apps/node35801/node.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
