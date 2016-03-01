e923a821d895cceda2dbaae028ec4aac8be0899f
mark reyes <mark.reyes@ucop.edu>
2016-03-01 13:15:40 -0800

    store prod

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,7 +3,7 @@
 JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2store/tomcat
-CATALINA_BASE=/apps/dpr2store/apps/storage35121/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_BASE=/dpr2store/apps/storage35121/tomcat
+CATALINA_OPTS="-Xmx2048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2store/apps/storage35121/storage.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
