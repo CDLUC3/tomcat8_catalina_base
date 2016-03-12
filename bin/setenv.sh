b4721458ce4a76a6f2021b204450991d45ad8910
mark reyes <mark.reyes@ucop.edu>
2016-03-11 16:45:37 -0800

    feeder prod

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,7 +3,7 @@
 JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
-CATALINA_ROOT=/apps/dpr2/apps/feeder33140
+CATALINA_ROOT=/dpr2/apps/feeder33140
 CATALINA_BASE=${CATALINA_ROOT}/tomcat
 CATALINA_OPTS="-Xmx512m -Xms256m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true -d64"
 CATALINA_PID=${CATALINA_ROOT}/feeder.pid
