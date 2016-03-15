31b88515f771b0bd5369a485a53466b363785cb4
mark reyes <mark.reyes@ucop.edu>
2016-03-15 16:16:20 -0700

    fix config

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,6 +4,6 @@ JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
 CATALINA_BASE=/dpr2/apps/storage35122/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_OPTS="-Xmx2048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2/apps/storage35122/storage.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
