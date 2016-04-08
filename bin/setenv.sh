bc66a6ccb0baa3e8b29c07a30f25286ab145cfdd
mark reyes <mark.reyes@ucop.edu>
2016-04-08 06:46:38 -0700

    increase size of Tomcat

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -5,7 +5,7 @@ JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
 CATALINA_ROOT=/apps/dpr2/apps/ingest33121
 CATALINA_BASE=${CATALINA_ROOT}/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms512m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true -d64"
+CATALINA_OPTS="-Xmx4096m -Xms2048m -XX:+UseG1GC -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true -d64"
 CATALINA_PID=${CATALINA_ROOT}/ingest.pid
 # CLASSPATH=${CATALINA_HOME}/bin/tomcat-juli.jar
 export JAVA_HOME JRE_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID CLASSPATH
