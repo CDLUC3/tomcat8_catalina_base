c0dd98f39dbe374086f87ab79797a282fd9e5dc4
Mark Reyes <mreyes@uc3-ingest01x2-stg.cdlib.org>
2020-07-21 16:17:27 -0700

    Now an i3 instance.  Increase JVM size

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,6 +4,6 @@ JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2store/tomcat
 CATALINA_BASE=/dpr2store/apps/storage35121/tomcat
-CATALINA_OPTS="-Xmx6g -Xms4g -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_OPTS="-Xmx12g -Xms8g -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2store/apps/storage35121/storage.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
