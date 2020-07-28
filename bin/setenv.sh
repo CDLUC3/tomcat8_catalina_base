ab0b8d457fd5b7c4d0d0ae6b2c1730b738e457ea
Mark Reyes <mreyes@uc3-ingest01x2-stg.cdlib.org>
2020-07-28 14:18:08 -0700

    rollback last commit

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,6 +4,6 @@ JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2store/tomcat
 CATALINA_BASE=/apps/dpr2store/apps/storage35121/tomcat
-CATALINA_OPTS="-Xmx12g -Xms8g -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_OPTS="-Xmx2048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2store/apps/storage35121/storage.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
