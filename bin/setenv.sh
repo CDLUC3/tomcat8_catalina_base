3c984d4c6daebacb0d8e685e780bfaeb741e4800
Mark Reyes <mreyes@uc3-ingest01x2-stg.cdlib.org>
2020-08-24 15:28:45 -0700

    Increase JVM to 3GB

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,6 +4,6 @@ JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
 CATALINA_BASE=/apps/dpr2/apps/audit37001/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_OPTS="-Xmx3072m -Xms3072m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2/apps/audit37001/audit.pid
 export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
