00ca8eec8230c973634c4eeb70e00a28d63c6f5f
Mark Reyes <mreyes@uc3-ingest01x2-stg.cdlib.org>
2022-05-09 14:15:04 -0700

    service based deployment

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,9 +1,11 @@
-# JAVA_HOME=/cdlcommon/products/java7
-# needed for G1 collector
 JAVA_HOME=/usr/lib/jvm/java
 JRE_HOME=/usr/lib/jvm/java
 CATALINA_HOME=/dpr2/tomcat
-CATALINA_BASE=/dpr2/apps/replic38001/tomcat
-CATALINA_OPTS="-Xmx2048m -Xms1024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
+CATALINA_BASE=/apps/dpr2/apps/replic38001/tomcat
+CATALINA_OPTS="-Xmx2048m -Xms2024m -Dfile.encoding=UTF8 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -XX:+UseG1GC -d64"
 CATALINA_PID=/dpr2/apps/replic38001/replic.pid
-export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID
+SSM_ROOT_PATH=/uc3/mrt/prd
+MERRITT_REPLIC_INFO=replic-info
+LOG_REPLIC=/apps/dpr2/apps/replic38001/tomcat
+NODE_TABLE=nodes-prod
+export JAVA_HOME CATALINA_HOME CATALINA_BASE CATALINA_OPTS CATALINA_PID SSM_ROOT_PATH MERRITT_REPLIC_INFO LOG_REPLIC NODE_TABLE
